16dc1cdb2e7be860c1e4db10b9801658ec57b471
AlfredoR <xbox.07@hotmail.com>
2021-03-16 14:03:16 -0600

    Front de form registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -8,12 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   public page_title: String;
+  public user: User;
 
   constructor() {
     this.page_title = 'Registrate';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+
+
+
+    // blic id: number,
+    // public name: string,
+    // public surname: string,
+    // public role: string,
+    // public email: string,
+    // public password: string,
+    // public description: string,
+    // public image: string
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    console.log(this.user);
+    form.reset()
   }
 
 }
